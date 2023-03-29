7772e0509335f31093618f0f2049fd89e8548119
Edwin Mwangi Njoroge <edwin.njoroge@student.moringaschool.com>
2023-03-29 05:15:27 +0300

    First deleiverable for initializers, readers and writers done

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,18 @@
 class Article
+    
+    @@all = []
+    attr_accessor :title, :magazine
+
+    def initialize(author, magazine, title)
+        @author = author
+        @magazine = magazine
+        @title = title
+        @@all << self
+    end
+    def self.all
+        @@all
+    end
+
 
 
 end
