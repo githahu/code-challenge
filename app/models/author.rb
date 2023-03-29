38d27ad6404b3488538af509fe9d36780f5e9df7
Edwin Mwangi Njoroge <edwin.njoroge@student.moringaschool.com>
2023-03-29 09:36:26 +0300

    returning array for article instances for an author and an array of mag instance as per author

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,11 +1,19 @@
 class Author
-  attr_accessor :name
-
+  attr_reader :name #change to attr_reader to disable changes after initialization
 
   def initialize(name)
     @name = name
   
   end
 
+  # to return an array of article instances an author has written
+  def articles
+    Articles.all.select { |article| article.author == self}
+  end
+  # to return a unique array of mag instances for which an author has contributed to
+
+  def magazines
+    articles.collect { |articles| article.magazine }.uniq
+  end
 
 end
